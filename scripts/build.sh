054bb8438f0ddf9d0311bbbb2c8b3dbf5fea51fe
norbix <norbert.jakubczak@gmail.com>
2024-12-04 22:18:18 +0100

    Improved build.sh wrapper for Dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "frontend" ]; then
     echo "Building frontend..."
     GOOS=js GOARCH=wasm go build -o ./build/main.wasm ./cmd/ui/main.go
+    cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./build/
     cp ./templates/index.html ./build/
     cp ./assets/* ./build/
     echo "Frontend build complete!"
